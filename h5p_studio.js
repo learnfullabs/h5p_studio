8398e3fe6c9495112feeb5f816885a83738962ea
ymdahi <ymdahi@gmail.com>
2024-01-20 00:52:35 +0000

    initial

---
+++ h5p_studio.js
@@ -0,0 +1,19 @@
+H5P.externalDispatcher.on('xAPI', function (event) {
+  
+  let h5pObject = {};
+
+  window.addEventListener('DOMContentLoaded', () => {  
+    h5pObject.maxScore = H5P.instances[0].getMaxScore();
+    parent.postMessage(JSON.stringify(h5pObject),"*")
+    
+  });
+  
+
+  parent.postMessage(JSON.stringify(event.data.statement),"*")
+
+  console.log(event.data.statement);
+  
+});
+
+
+console.log(H5P.isFramed);
